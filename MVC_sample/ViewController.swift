a2bc7efb2496239f63236b3dbe0bb5cd649b8f09
DaikiHiguchi <daikiit27@gmail.com>
2020-08-06 23:10:46 +0900

    Initial Commit

---
+++ MVC_sample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVC_sample
+//
+//  Created by 樋口大樹 on 2020/08/06.
+//  Copyright © 2020 樋口大樹. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
